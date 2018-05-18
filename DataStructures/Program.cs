a484251b1131b9231d93654c9abf2ad1881e347f
Ekaterina <kotoyama@list.ru>
2018-05-18 06:33:53 +0500

    init

---
+++ DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main()
+        {
+            var stack = new Stack<int>(new [] {1, 2, 3, 4, 5});
+            Console.WriteLine(stack.Peek());
+            stack.Pop();
+
+            foreach (var item in stack)
+                Console.Write(item + " ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructures/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class Stack<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private readonly List<T> _stack;
+        public int Count => _stack.Count;
+        public bool IsEmpty => _stack.Count == 0;
+
+        public Stack() => _stack = new List<T>();
+
+        public Stack(IEnumerable<T> items)
+        {
+            _stack = new List<T>();
+            foreach (var item in items)
+                Push(item);
+        }
+
+        public void Push(T item) => _stack.Add(item);
+
+        public T Peek() => IsEmpty ? throw new InvalidOperationException() : _stack[Count - 1];
+
+        public T Pop()
+        {
+            if (IsEmpty) throw new InvalidOperationException();
+            var item = _stack[Count - 1];
+            _stack.RemoveAt(Count - 1);
+            return item;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < Count; i++)
+                yield return _stack[i];
+        }
+    }
+}
